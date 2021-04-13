f2d3bcb6cda71c68599ecf8225c0d6263406c1f7
lepradenas <l.pradenasparedes@gmail.com>
2021-04-13 09:11:57 -0400

    carga inicial de pruebas de sistema

---
+++ Prueba4/Prueba4/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Prueba4.views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prueba4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage  = new MainPage();//--> descomentar para volver a main page
+            // se debe eliminar la primera para poder usar el segundo comando
+           // MainPage =  new NavigationPage(new Page1());
+
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Prueba4/Prueba4/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prueba4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        async private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
